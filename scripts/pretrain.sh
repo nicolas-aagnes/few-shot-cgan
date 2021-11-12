2a282bef5e0b0e7a8ca8193c6ddfb685aa9178d3
Nicolas Aagnes <nicolas.aagnes@gmail.com>
2021-11-11 16:33:27 -0800

    fix pretrain script

--- scripts/pretrain.sh
+++ scripts/pretrain.sh
@@ -1,7 +1,12 @@
-for datasetsize in 1000 5000 10000 50000
+# for datasetsize in 1000 5000 10000 50000
+# do
+#     for noiselevel in 0.01 0.05 0.1 0.15 0.2
+#     do
+#         python pretrain.py --dataset-size $datasetsize --noise-level $noiselevel
+#     done
+# done
+
+for noiselevel in 0.0 0.05 0.1 0.2 0.3 0.5
 do
-    for noiselevel in 0.01 0.05 0.1 0.15 0.2
-    do
-        python pretrain.py --dataset-size $datasetsize --noise-level $noiselevel
-    done
+    python pretrain.py --noise-level $noiselevel
 done
